67ef90c59e8815975b0231ece2f7699368193b58
bla <bla@bla.com>
2025-09-20 23:24:58 +0100

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,6 @@
 // --- CONFIGURATION ---
 // IMPORTANT: Replace this with your actual Web App URL from Google Apps Script deployment.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxZgeH78bFeiq3WGZdcVDLnqRj8mP3lzHitH4obhaQgDo8EQBtb1ixLLIzZsAZIV4siew/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyq3u_wwz3N4EW4n6ZHMeg5BO1tiWGRiUA7KQtix8W_eMzCBOq956aoFsjIHsAdZ9qB/exec';
 
 // --- GLOBAL EVENT LISTENERS ---
 document.addEventListener('DOMContentLoaded', function() {
