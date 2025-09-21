ef666107542062028f3bd847f74bcca25cf60bd7
bla <bla@bla.com>
2025-09-21 23:23:49 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// --- CONFIGURATION ---
+// IMPORTANT: Replace this with your actual Web App URL from Google Apps Script deployment.
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxZgeH78bFeiq3WGZdcVDLnqRj8mP3lzHitH4obhaQgDo8EQBtb1ixLLIzZsAZIV4siew/exec';
