065e84ace16669f666928bca3c18cce9336b1bfb
bla <bla@bla.com>
2025-09-21 17:49:31 +0100

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,3 @@
 // --- CONFIGURATION ---
 // IMPORTANT: Replace this with your actual Web App URL from Google Apps Script deployment.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyq3u_wwz3N4EW4n6ZHMeg5BO1tiWGRiUA7KQtix8W_eMzCBOq956aoFsjIHsAdZ9qB/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz_TLLhsg2e1zHfid09pFbEzwIguI7mmqtYcV-kYcqAvZx3xAPGDvbmSbcT8XJ_Zr4B/exec';
