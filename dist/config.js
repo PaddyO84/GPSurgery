e528f36e68c228527e094347f6a53b1e71ba355e
PaddyO84 <101278227+PaddyO84@users.noreply.github.com>
2025-09-21 18:30:46 +0100

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,3 @@
 // --- CONFIGURATION ---
 // IMPORTANT: Replace this with your actual Web App URL from Google Apps Script deployment.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz3OtCXTbKghdHuk4a7GI254OBzSxHyL9yxOivTMAxlJ66DBgkwuuQPz0wrzy4yVN4t/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwGzBWv5JSaP8koxgHqd8bxQwE5ElxUaxibOc1eoDUJcztsJzDj48eVIYwt7h3ZhZuu/exec';
