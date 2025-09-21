91cd52b5cb13fb0c7808eea955a6bfde209792f4
bla <bla@bla.com>
2025-09-21 23:10:57 +0100

    switching to v3

--- dist/config.js
+++
@@ -1,3 +0,0 @@
-// --- CONFIGURATION ---
-// IMPORTANT: Replace this with your actual Web App URL from Google Apps Script deployment.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby9MXuVc4rlJBzTORXW7j7TWdvMqbHzwSBjxdZXefOg1GVwKVOb2ZoTZZE4RMfseyPuew/exec';
