bf521f3350cb090ed0020a73acbff121072d1987
Patrick Noone <patricknoone@gmail.com>
2025-09-22 23:49:16 +0100

    update

--- dist/config.js
+++
@@ -1,3 +0,0 @@
-// --- CONFIGURATION ---
-// IMPORTANT: Replace this with your actual Web App URL from Google Apps Script deployment.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzUoOc71T7J-FR9CrZxuNlk32lhZvs-3x9mWd_jjjQ4U2WiMmBSlA1wLYJoJrzvTY7Hnw/exec';
